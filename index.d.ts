c0fbe1f33032aac3b30c17bc8085056cb87de843
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2019-02-10 03:33:00 +0900

    improve type definitions

--- index.d.ts
+++ index.d.ts
@@ -1,28 +1,6 @@
 import { ParsedUrlQuery } from 'querystring'
 
-export default function formatUrlQuery(
+export default function formatUrlQuery<T extends ParsedUrlQuery>(
   parsedUrlQuery: ParsedUrlQuery,
-  format: {
-    [key: string]: string
-  },
-): {
-  [key: string]: string
-}
-
-export default function formatUrlQuery(
-  parsedUrlQuery: ParsedUrlQuery,
-  format: {
-    [key: string]: string[]
-  },
-): {
-  [key: string]: string[]
-}
-
-export default function formatUrlQuery(
-  parsedUrlQuery: ParsedUrlQuery,
-  format: {
-    [key: string]: string | string[]
-  },
-): {
-  [key: string]: string | string[]
-}
+  format: T,
+): T
